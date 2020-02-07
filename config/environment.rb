03612b90a9d182ce48d27c11fea32d5a6fd3d455
github username <github email address>
2020-02-06 18:40:48 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,12 @@ require_all 'app/models'
 
 ENV["SINATRA_ENV"] ||= 'development'
 ActiveRecord::Base.establish_connection(ENV["SINATRA_ENV"].to_sym)
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/halloween.db"
+)
+
+require_relative '../lib/costume.rb'
+require_relative '../lib/costume_store.rb'
+require_relative '../lib/haunted_house.rb'
